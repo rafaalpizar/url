72831b6f112a0a18c0b143ca6f1b229006b88257
Rafael Alpizar <ralpizar@akamai.com>
2022-04-28 17:48:07 -0600

    Aditional special chars, change help funcion name to infohelp, read does not evaluate special chars

--- urlencodecsv.sh
+++ urlencodecsv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-help() {
+infohelp() {
     if [ $# -eq 0 ] && [ -z /dev/stdin ]; then
 	echo "This script transform text into URL encode."
 	echo "Its input form stdin or a file name, and its output is stdout"
@@ -12,18 +12,21 @@ help() {
 encodeline() {
     #[[ "${1}" ]] || echo "Line empty"
     local length="${#1}"
+    #echo Debug2: $1
     for (( i = 0; i < length; i++ )); do
         local c="${1:i:1}"
         case $c in
-	    [:,/%]) echo -n "$c" ;;
+	    '\') echo -n '\' ;;
+	    [+:,/%?=\&]) echo -n "$c" ;;
             [a-zA-Z0-9.~_-]) printf "$c" ;;
             *) printf '%s' "$c" | xxd -p -c1 | while read c; do printf '%%%s' "$c"; done ;;
         esac
     done
 }
 
-help
-while read line; do
+infohelp
+while read -r line; do
+    #echo Debug1: $line
     encodeline "$line"
     echo
 done < "${1:-/dev/stdin}"
