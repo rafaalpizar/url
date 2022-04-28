ff7e9391c4f4f98821862e267a2df93f0654a3f7
Rafael Alpizar <ralpizar@akamai.com>
2022-04-28 16:22:16 -0600

    Added script to encode with output compatible with paths and csv file

---
+++ urlencodecsv.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+help() {
+    if [ $# -eq 0 ]; then
+	echo "This script transform text into URL encode."
+	echo "Its input form stdin or a file name, and its output is stdout"
+	echo
+	exit 1
+    fi
+}
+
+
+encodeline() {
+    # urlencode <string>
+    [[ "${1}" ]] || return 1
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+	    [:,/%]) echo -n "$c" ;;
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%s' "$c" | xxd -p -c1 |
+                   while read c; do printf '%%%s' "$c"; done ;;
+        esac
+    done
+}
+
+	    # ,) printf "$c" ;;
+	    # /) printf "$c" ;;
+
+
+help
+while read line; do
+    encodeline "$line"
+    echo
+done < "${1:-/dev/stdin}"
